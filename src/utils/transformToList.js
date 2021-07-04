0be061cc950db9e6f38e59b37b18f16cdc3facc7
Piotr Cichowlas <cichowlas.p@gmail.com>
2021-07-04 21:28:01 +0200

    updated questions

--- src/utils/transformToList.js
+++ src/utils/transformToList.js
@@ -4,7 +4,7 @@ const pytania = `1.Czy kiedykolwiek wysyłałeś komuś sprośne zdjęcia?
 4.Czy kiedykolwiek odjebałeś dla kogoś taniec erotyczny? 
 5.Czy kiedykolwiek robiłeś sobie seksowne selfie?
 6.Czy kiedykoiwlek miałeś relacje "Friends with Benefits"?
-7.Czy kiedykolwiek całowałeś się z świeżo poznaną osobą?
+7.Czy kiedykolwiek całowałeś się z ledwo poznaną osobą?
 8.Czy kiedykolwiek pokazałeś komuś na szybko nagą część swojego ciała? (flashed someone)
 9.Czy kiedykolwiek odjebałeś trójkąt?
 10. Czy kiedykolwiek pierdolnąłęś trapez? 
@@ -85,7 +85,7 @@ const pytania = `1.Czy kiedykolwiek wysyłałeś komuś sprośne zdjęcia?
 85. Czy kiedykolwiek masturbowałeś się z pamięciówki?
 86. Czy kiedykolwiek masturbowałeś się do zdjęcia/ filmu kogoś kto robił sobie dobrze?
 87. Czy kiedykolwiek nadałeś komuś sprośną ksywkę?
-88. Czy kiedykolwiek rozmarzałyś się o waszej nowy poślubnej?
+88. Czy kiedykolwiek rozmarzałyś się o waszej nocy poślubnej?
 89. Czy kiedykolwiek nie nosiłaś stanika wychodzą z domu?
 90. Czy kiedykolwiek zmuszałeś Barbie i Kena do jebania się?
 91. Czy kiedykolwiek inscenizowałeś swoimi zabawkami, że odbywaja stosunek?
@@ -97,7 +97,210 @@ const pytania = `1.Czy kiedykolwiek wysyłałeś komuś sprośne zdjęcia?
 97. Czy kiedykolwiek ubrałaś/ubrałeś się jak niegrzeczna uczennnica?
 98. Czy kiedykolwiek podwijałaś sukienke, żeby była krótsza?
 99. Czy kiedykolwiek miałeś crusha na przyjaciela swojego chłopaka?
-100. Czy kiedykowliek ktoś śpiewał Ci serenady?`.split('\n');
+100. Czy kiedykowliek ktoś śpiewał Ci serenady?
+-
+101. Czy kiedykolwiek udawałeś orgazm?
+102. Czy kiedykolwiek nosiłeś bieliznę jako część ubrania?
+103. Czy kiedykolwiek ktoś Ci się oświadczył?
+104. Czy kiedykolwiek się komuś oświadczyłeś? 
+105. Czy kiedykolwiek spotkałeś sie z więcej niz jedną osobą jednego dnia?
+106. Czy kiedykolwiek podałeś komuś fałszywy numer telefonu?
+107. Czy kiedykolwiek obczajałeś/ obczajałaś tyłek faceta w jeansach?
+108. Czy kiedykolwiek pociągał Cię ktoś 10 lat starszy lub młodszy, od Ciebie?
+109. Czy kiedykolwiek ukrywałeś malinkę na szyji ubierając golf?
+110. Czy kiedykolwiek podnieciła Cię niepozorna scena w filmie?
+111. Czy kiedykolwiek wyszedłeś na miasto bez bielizny pod ubraniami?
+112. Czy kiedykolwiek pomyślałeś/ pomyślałaś, że koleżanka mamy jest hot?
+113. Czy kiedykolwiek poszedłeś do kościoła z zamiarem poznania kogoś?
+114. Czy kiedykolwiek kupiłeś dziewczynie pudełko czekoladek na walentynki?
+115. Czy kiedykolwiek ubierałeś się jak kobietka?
+116. Czy kiedykolwiek próbowałeś podglądać dziewczyny w damskiej przebieralni?
+117. Czy kiedykolwiek zapytałeś/ zapytałaś się kogoś o rękę?
+118. Czy kiedykolwiek miałeś gazetkę  Playboy w posiadaniu?
+119. Czy kiedykolwiek obsrałeś własną nogę na woodstocku? 
+120. Czy kiedykolwiek użyłeś tandetnego tekstu na podryw na kimś?
+121. Czy kiedykolwiek kupowałeś dziewczynie bieliznę na prezent? 
+122. Czy kiedykolwiek skończyłeś w łóżku z koleżanką swojej siostry?
+123. Czy kiedykolwiek pukałeś się wiecej niz raz dziennie?
+124. Czy kiedykolwiek pukałeś się więcej niz 2 razy dziennie? 
+125. Czy kiedykolwiek pukałeś się więcej niz 3 razy dziennie?
+126. Czy kiedykolwiek byłeś podduszany podczas seksu?
+127. Czy kiedykolwiek podduszałeś kogoś podczas jebania? 
+128. Czy lubisz być podduszany? 
+129. Czy lubisz ciągnięcie za włosy podczas pukania? 
+130. Czy kiedykolwiek oglądałeś Grę o Tron dla sen pukania?
+131. Czy kiedykolwiek uprawiałeś seks w miejscu publicznym? 
+132. Czy kiedykolwiek byłeś jebany w dupkę?
+133. Czy kiedykolwiek jebałeś kogoś w dupkę? 
+134. Czy kiedykolwiek ktoś robił Ci tatuaż? 
+135. Czy kiedykolwiek na zjaraniu próbowałeś odjebać kształt serca, ale ludzie nie wiedzieli w jaką stronę miało wyjść to serce? 
+136. Czy kiedykolwiek przyjechałeś na dziarkę i wieczór wczesniej spizgałeś się jak świnia?
+137. Czy kiedykolwiek wystawiłeś kogoś na randce/ spotkaniu na seks?
+138. Czy kiedykolwiek skłamałeś na temat czegoś żeby nie iść do pracy?
+139. Czy kiedykolwiek przedstawiłeś się komuś fałszywym imieniem?
+140. Czy kiedykolwiek rzuciłeś/ rzuciłaś kogoś przez wiadomość/sms?
+141. Czy kiedykolwiek wymiotowałeś w transporcie publicznym?
+142. Czy kiedykolwiek okłamałeś kogoś w tym kręgu? 
+143. Czy kiedykolwiek napisałeś do ex z dupy po jakimś czasie bez pisania? 
+144. Czy kiedykolwiek skłamałeś na aplikacji randkowej/ profilu randkowym?
+145. Czy kiedykolwiek całowałeś się z rodzieństwem swojego ziomeczka?
+146. Czy kiedykolwiek zrobiłeś strategicnzego softa lub nudesa w miejscu publicznym?
+147. Czy odjebałeś zajebisą apkę do losowania pytań do Hev U Hlebek
+148. Czy kiedykolwiek lizałeś się w kuchni z ziomkami na urodzinach brata?
+149. Czy kiedykolwiek spotkałeś się z ex swojego ziomka?
+150. Czy kiedykolwiek sikałeś pod prysznicem?
+151. Czy kiedykolwiek stalkowałeś nowego partnera swojego ex na social mediach?
+152. Czy kiedykolwiek złamałeś sobie coś?
+153. Czy kiedykolwiek byłeś zdradzany?
+154. Czy kiedykolwiek zdradziłeś kogoś?
+155. Czy kiedykolwiek przedobrzyłeś miodek i trafiłeś do szpitala i lekarz nie wiedział co przedawkowałeś jak mu tłumaczyłeś?
+156. Czy kiedykolwiek sciągałeś na tescie?
+157. Czy kiedykolwiek skłamałeś na temat całowania się z kimś?
+158. Czy kiedykolwiek złamałeś prawo?
+159. Czy kiedykolwiek sikałeś w miejscu publicznym? 
+160. Czy kiedykolwiek potajemnie przejzałeś kogoś wiadomości prywatne? 
+161. Czy kiedykolwiek używałeś kogoś konta na netflixie? 
+162. Czy kiedykolwiek edytowałeś swoje zdjęcia?
+163. Czy kiedykolwiek obejrzaeś jakąś serię za jednym podejściem?
+164. Czy kiedykolwiek telefon wpadł Ci do kibla? 
+165. Czy kiedykolwiek googlowałeś siebie w necie?
+166. Czy kiedykolwiek zapomniałeś gdzie zaparkowałeś swoje auto? 
+167. Czy kiedykolwiek oddałeś komuś w prezencie coś co sma dostałeś w prezecie bo tobie sie nie podobała?
+168. Czy kiedykolwiek wysłałeś sprośną wiadomość do złej osoby?
+169. Czy kiedykolwiek wysłałeś srpośną wiadomość specjalnie "przypadkiem" do złej osoby?
+170. Czy kiedykolwiek pukałeś się w morzu/ jeziorze albo basenie?
+171. Czy kiedykolwiek miziałeś sie z kimś w morzu/ jeziorze albo basenie?
+172. Czy kiedykolwiek spałeś ze współpracownikiem?
+173. Czy kiedykolwiek ssałeś palce u stóp swojego partnera? 
+174. Czy kiedykolwiek miałeś sprośny sen o kimś w tym kręgu? 
+175. Czy kiedykolwiek załujesz jakiegoś zbliżenia seksualnego/ doświadczenia seksualnego?
+176. Czy kiedykolwiek googlowałeś sex pozycje?
+177. Czy kiedykolwiek całowałeś się z więcej niż jedną osobą tego samego dnia? 
+178. Czy kiedykolwiek poszedłeś na randkę i nie miałeś zamiaru płacić za jedzenie czy cokolwiek?
+179. Czy kiedykolwiek udawałeś, że dostałeś wiadomość zeby się skąś ulotnić? 
+180. Czy kiedykolweik byłeś dla kogoś nahalny?
+181. Czy kiedykolwiek używałeś kajdanek podczas pukania?
+182. Czy kiedykolwiek odjebałeś helikopter publicznie?
+183. Czy kiedykolwiek odkurwiłeś oczy ważki?
+184. Czy kiedykolwiek pierdolnąłeś puzon?
+185. Czy kiedykolwiek podjebałeś kogoś szczoteczkę do zębów do czegos innego niż mycie zębów?
+186. Czy kiedykolwiek spotykałeś się z kimś i utrzymywałes to w sekrecie?
+187. Czy kiedykolwiek tańczyłeś na barze?
+188. Czy kiedykolwiek byłeś aresztowany? 
+189. Czy kiedykolwiek grałeś w lizaną butelkę?
+190. Czy kiedykolwiek nie umyłeś rąk po dwójce?
+191. Czy kiedykolwiek zrzuciłeś puszczenie bąka na zwierze? 
+192. Czy kiedykolwiek posmarkałeś sobie ręcę a potem wytarłeś w cokowiek co było obok?
+193. Czy kiedykolwiek posikałeś łóżko majać już ponad 18 lat?
+194. Czy kiedykolwiek lizałeś się w jadącej windzie?
+195. Czy kiedykolwiek używałeś czegoś jako sekszabawka co nie było sekszabawką?
+196. Czy kiedykolwiek udawałeś coś więcej w stosunku do bliskiej osoby, żeby odkryć czy ona czuje coś więcej do ciebie?
+197. Czy kiedykolwiek miałeś przygodę oralną z kimś z kim nie całowałeś się w usta wcale?
+198. czy kiedykowliek doznałeś kontuzji albo podraznienia po uzywaniu zabawek erotycznych?
+199. Czy kiedykolwiek sąsiedzi dobijali się do mnie po głośnej sesji pukania?
+200. Czy kkiedykowiek całowałeś się z kimś gdy w międzyczasie dolewali Ci alkoholu?
+-
+201. Czy kiedykolwiek zjebałeś się podczas pukania?
+202. Czy kiedykolwiek zjebałeś się podczas całowania z kimś?
+203. Czy kiedykolwiek zdejmowałeś komuś część ubrania ustami lub zębami?
+204. Czy kiedykolwiek zaprosiłeś "ziomka" do domu kiedy byłaś sama/sam?
+205. Czy kiedykolwiek obudziłeś się obok nieznajomego? 
+206. Czy kiedykolwiek wąchałeś kobiece majtki?
+207. Czy kiedykolwiek marzyłeś o pukaniu się w wannie lub basenie?
+208. Czy kiedykolwiek miałeś więcej niż jeden orgazm podczas jednej sesji pukania?
+209. Czy kiedykolwiek pukałeś się bez gumy?
+210. Czy kiedykolwiek podwoziłeś nieznajomego stojącego przy drodze?
+211. Czy kiedykolwiek ktoś mnie przekupił, żebym skłamał w jakiejs sprawie? 
+212. Czy kiedykolwiek przelizałem się z kimś na imprezie w klubie?
+213. Czy kiedykolwiek oglądałeś pornola w czasie lekcji?
+214. Czy kiedykolwiek podniecił Cię jakiś seksowny teledysk?
+215. Czy kiedykolwiek wyjechałeś na zagraniczny wyjazd z paczką gumek licząc na jebanie?
+216. Czy kiedykolwiek pukałeś się bez rozbierania się?
+217. Czy kiedykolwiek byłeś nie od końca usatysfakcjonowany z seksu?
+218. Czy kiedykolwiek pukałeś się z kimś gdy w pomieszczeniu był ktoś jeszcze?
+219. Czy kiedykolwiek smakowałeś wydzieline kogoś innego?
+220. Czy kiedykowliek obserwowałeś inną parę gdy się do siebie dobierała?
+221. Czy kiedykolwiek goliłeś włosy łonowe swojego partnetra?
+222. Czy kiedykolwiek rodzice wieszli do pokoju podczas kiedy byłeś podczas zblizenia?
+223. Czy kiedykolwiek zlizywałeś z kogoś jedzenie?
+224. Czy kiedykolwiek zabawiałeś się z kimś w aucie i przypadkowo włączyłeś klakson?
+225. Czy kiedykolwiek dawałeś przyjemność swojemu martnerowi dłuzej niz pół godziny?
+226. Czy kiedykolwiek miałeś brudne myśli o kimś z kręgu?
+227. Czy kiedykolwiek brałeś prysznic z osobą przeciwnej płci? 
+228. Czy kiedykolwiek brałeś prysznic z osobą tej samej płci?
+229. Czy kiedykolwiek włożyłeś palec w czyj odbyt?
+230. Czy kiedykolwiek próbowałeś swoich własnych wydzielin?
+231. Czy w zeszłym roku byłeś osobą która piła najmniej w Hev u Hlebek?
+232. Czy kiedykolwiek odbyłeś sesje pukania bez kładzenia się na łóżko?
+233. Czy kiedykolwiek musiałeś uprać ubranie bo ktoś je pobrudził podczas puaknia?
+234. Czy kiedykolwiek spałeś w łóżku z więcej niż pięcioma osobami?
+235. Czy kiedykolwiek poleciałeś dziko podczas jakiejś imprezy?
+236. KARTA PUŁAPKA! Krąg wymyśla pytanie tak żeby wypiły tylko dwie osoby! 
+237. Czy kiedykolwiek zabawiałeś się z kimś w pokoju swojego rodzeństwa? 
+238. Czy kiedykolwiek obudziłeś się i ktoś opierdalał Ci gałe?
+239. Czy kiedykolwiek miałeś pociąg seksualny do zwierząt? 
+240. Czy kiedykolwiek robiłeś komuś nudesy?
+241. Czy kiedykolwiek pukałeś sie w toalecie?
+242. Czy kiedykolwiek robiłeś to w śpiworze? 
+243. Czy kiedykolwiek nie umyłeś jakiejś częsci ubrania ponieważ pachniała kimś?
+244. Czy kiedykolwiek używałeś lubrykantu? 
+245. Czy kiedykolwiek płakałeś na filmie Pixara?
+246. Czy kiedykolwiek byłeś ze złą osobą w złym czasie?
+247. Czy kiedykolwiek dałeś komuś z pięsci w ryj?
+248. Czy kiedykolwiek wylizywałeś anusik?
+249. Czy kiedykolwiek włożyłeś na siebie brudną bieliznę bo byłeś zbyt leniwy żeby zrobić pranie?
+250. Czy kiedykolwiek miałeś tripa w miejscu publicznym i udawałeś że tak ma być?
+251. Czy kiedykolwiek byłeś piany w kościele?
+252. Czy kiedykolwiek zacząłeś jeść podczas płaczu?
+253. Czy kiedykolwiek postrzeliłeś kogoś przypadkowo?
+254. Czy kiedykolwiek obciąłeś sobie sam włosy?
+255. Czy kiedykolwiek powiedziałeś, że już nigdy więcej nie będziesz pić?
+256. Czy kiedykolwiek paliłeś jakiekolwiek substancje?
+257. Czy kiedykolwiek czytałeś ksiązki? 
+258. Czy kiedykolwiek czytałeś ksiązki bez fabuły?
+259. Czy kiedykolwiek spałeś ze swoim zwierzakiem?
+260. Czy kiedykolwiek musiałeś brać tabletkę "dzień po"?
+262. TRIGGEER WARNING !Czy kiedykolwiek zrzygałeś się i potem to połknąłeś?
+263. Czy kiedykolwiek pukałeś się pod prysznicem?
+264. Czy kiedykolwiek bałeś się ciemności?
+265. Czy kiedykolwiek wspinałeś się na drzewo?
+266. Czy kiedykolwiek nie zdałeś semestru z czegoś?
+267. Czy kiedykolwiek zasnąłeś w transporcie publicznym i przejechałeś swój przystanek?
+268. Czy kiedykolwiek połknąłeś gumę do rzucia?
+269. Czy kiedykolwiek powątpiewałeś w swoją oriętacje seksualną?
+270. Czy kiedykolwiek próbowałeś zaimponować osobie która Cię nieniawidziła?
+271. Czy kiedykolwiek śpiewałeś Barke?
+272. Czy was kiedyś PIS ruchał?
+273. Czy kiedykolwiek wstawiłeś na swój profil zdjęcie kóre Horbik robiła analogiem?
+274. Czy kiedykolwiek byliście na strajku kobiet?
+275. Czy kiedykolwiek piłeś Monsterka?
+276. Czy Harnaś to król gór?
+277. Czy od lat męczysz żart o robieniu nożyc?
+278. Czy kiedykolwiek miałeś telegazetę?
+279. Czy kiedykolwiek miałeś telegazetę w kurtce Ciotki Noel?
+280. Czy kiedykolwiek miałeś tatuaż z automatu na dupie?
+281. Czy kiedykoliwek zerowałeś Amarenę?
+282. Czy kiedykolwiek wkładałeś koledze puszki w spodnie?
+283. Czy kiedykolwiek nagrywałeś FMMA i wyjebałeś Monsterka?
+284. Czy kiedykolwiek rzygałeś na woodzie i wypiłeś 3 butelki wody?
+285. Czy uważasz, że koty są lepsze niż psy?
+286. Czy uważasz, że psy są lepsze niż koty? 
+287. Czy kiedykolwiek poznałeś kota o najlpeszym imieniu Żurek?
+288. Czy kiedykolwiek miałeś swoją własną drukarnie?
+289. Czy kiedykolwiek ubierałeś się w strój Wonder Women?
+290. Czy kiedykolwiek zrobiłeś sobie zdjęcie ze swoimi rzygami?
+291. Czy kiedykolwiek odjebałeś Ricardo?
+292. Czy kiedykolwiek byłeś spisany?
+293. Czy kiedykolwiek wrzuciłeś coś do Heńczok?
+294. Czy kiedykolwiek kupiłeś coś na Alieexpres?
+295. Czy kiedykolwiek rozjebałeś krzesełko Lewka?
+296. Czy kiedykolwiek widziałeś jajka Mikesa?
+297. Czy kiedykolwiek czytałeś TT Martyny?
+298. Czy kiedykolwiek czytałeś książki ze słoika po pesto?
+299. Czy kiedykolwiek narzekałeś na ceny lub kierownictwo w Cyber Machinie?
+300. Czy kiedykolwiek wziąłeś jakieś ubranie z kosza na brudy, żeby je założyć?`.split(
+	'\n'
+);
 
 export const tranformedPytania = pytania.map((el, index) => {
 	return { index: index, question: el.replace(/[0-9.]/g, '').trim() };
